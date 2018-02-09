aa052449336df447a4c87e9e740f7f36de8e2cbd
svijayvargiy <35984624+svijayvargiy@users.noreply.github.com>
2018-02-09 11:34:03 +0530

    Update License Text

--- src/vmw_conn_main.c
+++ src/vmw_conn_main.c
@@ -3,7 +3,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation (or any later at your option)
+ * Free Software Foundation; version 2.
 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -17,7 +17,7 @@
  */
 
 /*
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: GPL-2.0-only
  */
 
 /*
